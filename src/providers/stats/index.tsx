18456429f5d784b483a262e5eb8c176157f92a9f
ngundotra <noah@gundotra.org>
2023-02-10 10:42:40 -0500

    delete everything & import new README & package.json files

--- src/providers/stats/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { SolanaClusterStatsProvider } from "./solanaClusterStats";
-
-type Props = { children: React.ReactNode };
-export function StatsProvider({ children }: Props) {
-  return <SolanaClusterStatsProvider>{children}</SolanaClusterStatsProvider>;
-}
