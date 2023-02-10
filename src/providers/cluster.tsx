a369987464f18edd210935710a38c81e613e36cb
ngundotra <noah@gundotra.org>
2023-02-10 17:40:31 -0500

    full parity with solana-labs/solana's explorer

--- src/providers/cluster.tsx
+++ src/providers/cluster.tsx
@@ -73,9 +73,7 @@ export function clusterUrl(cluster: Cluster, customUrl: string): string {
     case Cluster.Devnet:
       return modifyUrl(DEVNET_URL);
     case Cluster.MainnetBeta:
-      return (
-        process.env.REACT_APP_MAINNET_RPC_URL ?? modifyUrl(MAINNET_BETA_URL)
-      );
+      return modifyUrl(MAINNET_BETA_URL);
     case Cluster.Testnet:
       return modifyUrl(TESTNET_URL);
     case Cluster.Custom:
