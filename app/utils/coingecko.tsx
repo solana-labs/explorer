61a754317f20981515479a90f810be7f49cba23a
Steven Luscher <steven.luscher@solana.com>
2023-05-11 06:09:51 +0000

    Don't poll for data when the tab is in the background # Summary This should relieve pressure on the RPCs quite a bit
    # Test Plan
    
    1. Load localhost:3000
    2. Open network pane and watch RPC polling requests
    3. Switch to a different tab
    4. Return to Explorer
    
    Observe that no network requests were made while you were in the background, but the polling resumes when you return

--- app/utils/coingecko.tsx
+++ app/utils/coingecko.tsx
@@ -2,6 +2,7 @@
 // @ts-ignore
 import * as CoinGecko from 'coingecko-api';
 import React from 'react';
+import useTabVisibility from 'use-tab-visibility';
 
 const PRICE_REFRESH = 10000;
 
@@ -48,35 +49,41 @@ export type CoinGeckoResult = {
 
 export function useCoinGecko(coinId?: string): CoinGeckoResult | undefined {
     const [coinInfo, setCoinInfo] = React.useState<CoinGeckoResult>();
+    const { visible: isTabVisible } = useTabVisibility();
     React.useEffect(() => {
+        if (!isTabVisible) {
+            return;
+        }
         let interval: NodeJS.Timeout | undefined;
+        let stale = false;
         if (coinId) {
-            const getCoinInfo = (refresh = false) => {
+            const getCoinInfo = async (refresh = false) => {
                 if (!refresh) {
                     setCoinInfo({
                         status: CoingeckoStatus.Loading,
                     });
                 }
-                CoinGeckoClient.coins
-                    .fetch(coinId)
-                    .then((info: CoinInfoResult) => {
-                        setCoinInfo({
-                            coinInfo: {
-                                last_updated: new Date(info.data.last_updated),
-                                market_cap: info.data.market_data.market_cap.usd,
-                                market_cap_rank: info.data.market_data.market_cap_rank,
-                                price: info.data.market_data.current_price.usd,
-                                price_change_percentage_24h: info.data.market_data.price_change_percentage_24h,
-                                volume_24: info.data.market_data.total_volume.usd,
-                            },
-                            status: CoingeckoStatus.Success,
-                        });
-                    })
-                    .catch((_error: any) => {
-                        setCoinInfo({
-                            status: CoingeckoStatus.FetchFailed,
-                        });
+                try {
+                    const info: CoinInfoResult = await CoinGeckoClient.coins.fetch(coinId);
+                    if (stale) {
+                        return;
+                    }
+                    setCoinInfo({
+                        coinInfo: {
+                            last_updated: new Date(info.data.last_updated),
+                            market_cap: info.data.market_data.market_cap.usd,
+                            market_cap_rank: info.data.market_data.market_cap_rank,
+                            price: info.data.market_data.current_price.usd,
+                            price_change_percentage_24h: info.data.market_data.price_change_percentage_24h,
+                            volume_24: info.data.market_data.total_volume.usd,
+                        },
+                        status: CoingeckoStatus.Success,
                     });
+                } catch {
+                    setCoinInfo({
+                        status: CoingeckoStatus.FetchFailed,
+                    });
+                }
             };
 
             getCoinInfo();
@@ -88,8 +95,9 @@ export function useCoinGecko(coinId?: string): CoinGeckoResult | undefined {
             if (interval) {
                 clearInterval(interval);
             }
+            stale = true;
         };
-    }, [setCoinInfo, coinId]);
+    }, [coinId, isTabVisible]);
 
     return coinInfo;
 }
