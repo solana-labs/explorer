748a6f1829f1cacb2110df97ad34b834e5e66f9d
Kyle Espinola <kyle.s.espinola@gmail.com>
2024-07-28 08:40:32 +0200

    chore: change zumanet cluster

--- explorer.config.ts
+++ explorer.config.ts
@@ -3,15 +3,11 @@ const explorer = {
         {
             cluster: 'zumanet',
             name: 'Zumanet',
-            uri: 'http://145.40.115.243:8899',
+            uri: 'https://zumanet.abklabs.com',
         },
     ],
     features: {
         pingStats: false,
-    },
-    metadata: {
-        description: 'Explorer is a simple file explorer',
-        title: 'Explorer',
     },
 };
 
