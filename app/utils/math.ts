838259bb26605b1695d478ca7b777a9d20979529
FarinM <farin681@gmail.com>
2024-08-03 02:37:49 +0200

    Handle division by zero in percentage function

--- app/utils/math.ts
+++ app/utils/math.ts
@@ -5,6 +5,9 @@
 export function percentage(numerator: bigint, denominator: bigint, decimals: number): number {
     // since bigint is integer, we need to multiply first to get decimals
     // see https://stackoverflow.com/a/63095380/1375972
+    if (denominator === 0n) {
+        return 0
+    }
     const pow = 10 ** decimals;
     return Number((numerator * BigInt(100 * pow)) / denominator) / pow;
 }
