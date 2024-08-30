8229ee38f3887fd7c3ba1dc974efa701c1746abd
Hardhat Chad <155858888+HardhatChad@users.noreply.github.com>
2024-08-30 08:54:18 -0500

    Add ORE program (#360)
    Adds ORE program mainnet id
    
    ---------
    
    Co-authored-by: Noah Gundotra <ngundotra@users.noreply.github.com>

--- app/utils/programs.ts
+++ app/utils/programs.ts
@@ -49,6 +49,7 @@ export enum PROGRAM_NAMES {
     ORCA_SWAP_1 = 'Orca Swap Program v1',
     ORCA_SWAP_2 = 'Orca Swap Program v2',
     ORCA_AQUAFARM = 'Orca Aquafarm Program',
+    ORE = 'ORE Program',
     PORT = 'Port Finance Program',
     PYTH_DEVNET = 'Pyth Oracle Program',
     PYTH_TESTNET = 'Pyth Oracle Program',
@@ -409,6 +410,10 @@ export const PROGRAM_INFO_BY_ID: { [address: string]: ProgramInfo } = {
     namesLPneVptA9Z5rqUDD9tMTWEJwofgaYwp8cawRkX: {
         deployments: LIVE_CLUSTERS,
         name: PROGRAM_NAMES.NAME,
+    },
+    oreV2ZymfyeXgNgBdqMkumTqqAprVqgBWQfoYkrtKWQ: {
+        deployments: [Cluster.MainnetBeta],
+        name: PROGRAM_NAMES.ORE
     },
     p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98: {
         deployments: LIVE_CLUSTERS,
